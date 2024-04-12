bd0fa3bef71c8aad99a2f9010f99c4d89e2cd472
sachinxsharma <sachinsharma72003@gmail.com>
2024-04-12 16:39:03 +0530

    created a todo app using state variables nd extra feature adding new todo by clicking on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+      title: "Go to gym",
+      description: "Go to gym from 7-9",
+      completed: false
+    },
+    {
+      title: "Study DSA",
+      description: "From 9-10",
+      completed: false
+    }
+  ]);
 
-
-  function onClickHandler(){
-    setCount(count+1)
-    console.log(setCount)
+  function addTodo() {
+    setTodos([...todos, {
+      title: "new title ",
+      description: "desc of new todo"
+    }]);
   }
+
   return (
-    <>
-      <div>
-      <button onClick={onClickHandler}>Counter {count}</button>
-      </div>
-    </>
-  )
+    <div>
+      <button onClick={addTodo}>Add a random todo</button>
+      {todos.map((todo, index) => (
+        <Todo key={index} title={todo.title} description={todo.description} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+function Todo(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  );
+}
+
+export default App;
