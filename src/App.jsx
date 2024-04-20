84534d312f432868eac88504dfc166c1687cdea8
sachinxsharma <sachinsharma72003@gmail.com>
2024-04-20 13:58:30 +0530

    created a todo app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,61 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      title: "Go to gym",
-      description: "Go to gym from 7-9",
-      completed: false
-    },
-    {
-      title: "Study DSA",
-      description: "From 9-10",
-      completed: false
+let counter = 4;
+function App(){
+  const [todos, setTodos] = useState([{
+    id:1,
+    title:"go to gym",
+    description:"go to gym today"
+  },{
+    id:2,
+    title:"go to office",
+    description:"go to office today"
+  }, {
+    id:3,
+    title:"go to college",
+    description:"go to college today"
+  }])
+
+  function addTodo(){
+    // setTodos([...todos, {
+    //   id:4,
+    //   title: Math.random(),
+    //   description: Math.random()
+    // }])
+
+    const newTodos = [];
+    for (let i =0; i<todos.length; i++){
+      newTodos.push(todos[i]);
     }
-  ]);
+    newTodos.push({
+      id:counter++,
+      title:Math.random(),
+      description: Math.random()
+    })
+    setTodos(newTodos)
 
-  function addTodo() {
-    setTodos([...todos, {
-      title: "new title ",
-      description: "desc of new todo"
-    }]);
   }
 
   return (
     <div>
-      <button onClick={addTodo}>Add a random todo</button>
-      {todos.map((todo, index) => (
-        <Todo key={index} title={todo.title} description={todo.description} />
-      ))}
+      <button onClick={addTodo}>Add a todo</button>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} /> )}
+      {/* {todos.map(function(todo){
+        return <Todo title={todo.title} description={todo.description} /> 
+      })} */}
     </div>
-  );
+  )
 }
 
-function Todo(props) {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <h2>{props.description}</h2>
-    </div>
-  );
+function Todo({title, description}){
+  return <div>
+    <h1>
+      {title}
+    </h1>
+    <h5>
+      {description}
+    </h5>
+  </div>
 }
 
-export default App;
+export default App
