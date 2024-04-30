9319b477bd2089aefc7d6e519197c439b5efb43b
sachinxsharma <sachinsharma72003@gmail.com>
2024-04-30 11:34:10 +0530

    played with axios and its dependancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from 'axios';
 
 function App(){
-  const[count, setCount] = useState(0)
-
+  const [selectedId, setSelectedId] = useState(1);
   return <div>
+  <button onClick={function(){
+    setSelectedId(1);
+  }}>1</button>
     <button onClick={function(){
-      setCount(count+1);
-    }}>Click me {count}</button>
+    setSelectedId(2);
+  }}>2</button>
+    <button onClick={function(){
+    setSelectedId(3);
+  }}>3</button>
+    <button onClick={function(){
+    setSelectedId(4);
+  }}>4</button>
+    <Todo id ={selectedId} />
   </div>
 }
+
+function Todo({id}){
+  const [todo, setTodo] = useState({});
+
+  useEffect(()=>{
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      .then(response =>{
+        setTodo(response.data.todo)
+      })
+  },[id])
+
+
+
+  return <div>
+  Id:{id}
+    <h1>
+      {todo.title}
+    </h1>
+    <h4>
+      {todo.description}
+    </h4>
+  </div>
+}
+
 export default App;
