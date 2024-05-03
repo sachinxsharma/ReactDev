75c8f1f8287c77da1b1a721cc934c961645b2855
sachinxsharma <sachinsharma72003@gmail.com>
2024-05-03 11:59:54 +0530

    Reconciliation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [inputValue, setInputValue] = useState(1);
-
-  let count = useMemo(() => {
-    let count = 0;
-    for (let i = 1; i <= parseInt(inputValue); i++) { // Convert inputValue to a number
-      count += i;
-    }
-    return count;
-  }, [inputValue]);
+function App(){
+  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <input
-        type="number" // Specify input type as number
-        value={inputValue}
-        onChange={(e) => {
-          setInputValue(e.target.value);
-        }}
-        placeholder={"find sum from 1 to n"}
-      />
-      <br />
-      sum from 1 to {inputValue} is {count}
-      <br />
-      <button onClick={() => {
-        setCounter(counter + 1);
-      }}>counter {counter}
-      </button>
+      <button onClick={function(){
+        setCount(count + 1)
+      }}
+      
+      >count is {count}</button>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App; 
